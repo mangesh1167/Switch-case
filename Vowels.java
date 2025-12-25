426da2659ce36333ad85852e2ffcebb8722064b0
mangesh1167 <mangeshchavan1167@gmail.com>
2025-12-25 21:39:47 +0530

    Add files via upload

---
+++ Vowels.java
@@ -0,0 +1,37 @@
+package controlflowstatements;
+
+public class Vowels {
+
+		public static void main(String[] args) {
+
+		char vowel = 'a';
+		switch(vowel) {
+		
+		case 'a':
+			System.out.println("vowel");
+			break;
+			
+		case 'e':
+			System.out.println("vowel");
+			break;
+		
+		case 'i':
+			System.out.println("vowel");
+			break;
+			
+		case 'o':
+			System.out.println("vowel");
+			break;
+			
+		case 'u':
+			System.out.println("vowel");
+			break;
+			
+			default:
+				System.out.println("consonant");
+		
+
+		}	            
+	}
+}
+
