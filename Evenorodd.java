92091a06562d47151657641f41ebf460ce574b2e
mangesh1167 <mangeshchavan1167@gmail.com>
2025-12-26 00:13:06 +0530

    Add files via upload

---
+++ Evenorodd.java
@@ -0,0 +1,24 @@
+package controlflowstatements;
+
+public class Evenorodd {
+
+		public static void main(String[] args) {
+			
+			int num = 5;
+			switch(num % 2) {
+			  
+			case 1: 
+				System.out.println("Odd");
+				break;
+				
+			case 2:
+				System.out.println("Even");
+				break;
+				
+			 
+			
+		}
+
+	}
+}
+
